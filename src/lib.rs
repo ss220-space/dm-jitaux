9013fa999e928d680fd8faf1f1ee149048c6455d
Simon Ogorodnik <simon.ogorodnik@jetbrains.com>
2021-11-30 04:05:24 +0300

    Move tool-like procs to separate mod

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,9 @@ mod time;
 #[cfg(feature = "bench_utils")]
 mod bench_utils;
 
+#[cfg(feature = "tools")]
+mod tools;
+
 
 #[macro_use]
 extern crate auxtools;
@@ -29,12 +32,9 @@ use auxtools::hooks::call_counts;
 
 
 use log::LevelFilter;
-use std::collections::HashMap;
-use std::borrow::{BorrowMut, Borrow};
 use std::panic::{UnwindSafe, catch_unwind};
-use dmasm::{format_disassembly, Instruction};
+use dmasm::format_disassembly;
 use std::process::exit;
-use dmasm::operands::Variable;
 
 
 pub struct DisassembleEnv;
@@ -112,83 +112,6 @@ pub fn dump_call_count() {
         log::info!("Total {} procs", vec.len());
         for count in vec {
             log::info!("{}\t{}", count.count, count.proc.path);
-        }
-    }
-    Ok(Value::null())
-}
-
-pub fn var_desc(v: &Variable) -> String {
-    match v {
-        Variable::Null => "Null",
-        Variable::World => "World",
-        Variable::Usr => "Usr",
-        Variable::Src => "Src",
-        Variable::Args => "Args",
-        Variable::Dot => "Dot",
-        Variable::Cache => "Cache",
-        Variable::CacheKey => "CacheKey",
-        Variable::CacheIndex => "CacheIndex",
-        Variable::Arg(_) => "Arg",
-        Variable::Local(_) => "Local",
-        Variable::Global(_) => "Global",
-        Variable::SetCache(a, b) => return format!("SetCache({}, {})", var_desc(a.borrow()), var_desc(b.borrow())),
-        Variable::Initial(_) => "Initial",
-        Variable::IsSaved(_) => "IsSaved",
-        Variable::Field(_) => "Field",
-        Variable::StaticVerb(_) => "StaticVerb",
-        Variable::DynamicVerb(_) => "DynamicVerb",
-        Variable::StaticProc(_) => "StaticProc",
-        Variable::DynamicProc(_) => "DynamicProc",
-    }.to_string()
-}
-
-#[hook("/proc/dmjit_dump_opcode_count")]
-pub fn dump_opcode_count() {
-    log::info!("[DOC] Dump opcode counts");
-    if let Some(mut vec) = call_counts() {
-        vec.sort_by_key(|h| h.count);
-        log::info!("[DOC] Total {} procs", vec.len());
-
-        let mut env = DisassembleEnv {};
-
-        let mut map: HashMap<String, u64> = HashMap::new();
-
-        for count in vec {
-            unsafe {
-                let (nodes, res) = dmasm::disassembler::disassemble(count.proc.bytecode(), &mut env);
-                for node in nodes {
-                    match node {
-                        dmasm::Node::Instruction(insn, _) => {
-
-                            let mut name = insn.op_name();
-                            match insn {
-                                Instruction::Call(v, _) => {
-                                    name = format!("{} {}", name, var_desc(&v))
-                                },
-                                Instruction::CallStatement(v, _) => {
-                                    name = format!("{} {}", name, var_desc(&v))
-                                },
-                                Instruction::GetVar(v) => {
-                                    name = format!("{} {}", name, var_desc(&v))
-                                }
-                                Instruction::SetVar(v) => {
-                                    name = format!("{} {}", name, var_desc(&v))
-                                }
-                                _ => {}
-                            }
-
-                            *map.entry(name).or_default().borrow_mut() += u64::from(count.count)
-                        }
-                        _ => {}
-                    }
-                }
-            }
-        }
-
-        let mut res = map.iter().collect::<Vec<_>>();
-        res.sort_by_key(|e| -(*e.1 as i64));
-        for (op, count) in res {
-            log::info!("[DOC] {}\t{}", count, op);
         }
     }
     Ok(Value::null())
