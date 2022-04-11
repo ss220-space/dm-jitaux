5a203477dc1be9e1719f2f5edfaa3bf20394555e
simon.ogorodnik <simon.ogorodnik@jetbrains.com>
2022-04-11 06:04:58 +0300

    Improve signature_utils, allow to access variables properly

--- src/pads/signature_utils.rs
+++ src/pads/signature_utils.rs
@@ -3,13 +3,80 @@ pub struct ExSignature {
     pub marker_pos: u32
 }
 
+use std::ops::{Deref, DerefMut};
 use auxtools::sigscan::Scanner;
 use itertools::Itertools;
 use once_cell::sync::Lazy;
 use dmjit_macro::ex_signature;
 
-pub(crate) static SCANNER: Lazy<Scanner> = Lazy::new(|| {
-    auxtools::sigscan::Scanner::for_module(auxtools::BYONDCORE).unwrap()
+pub(crate) struct ForceSyncSend<T: Sized>(T);
+
+unsafe impl<T: Sized> Sync for ForceSyncSend<T> {}
+unsafe impl<T: Sized> Send for ForceSyncSend<T> {}
+
+impl<T: Sized> Deref for ForceSyncSend<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+pub(crate) struct DynamicBoundVariable<T: Sized>(Lazy<*mut T>);
+unsafe impl<T: Sized> Sync for DynamicBoundVariable<T> {}
+unsafe impl<T: Sized> Send for DynamicBoundVariable<T> {}
+
+impl<T: Sized> Deref for DynamicBoundVariable<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &**self.0 }
+    }
+}
+
+impl<T: Sized> DerefMut for DynamicBoundVariable<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { &mut **self.0 }
+    }
+}
+
+impl<T: Sized> Into<*mut T> for &DynamicBoundVariable<T> {
+    fn into(self) -> *mut T {
+        *self.0.deref()
+    }
+}
+
+impl<T> DynamicBoundVariable<T> {
+    pub(crate) const fn new(init: fn() -> *mut T) -> Self {
+        Self(Lazy::new(init))
+    }
+    pub(crate) fn init(&self) {
+        Lazy::force(&self.0);
+    }
+}
+
+pub(crate) struct DynamicBoundFunction<T: Sized>(Lazy<T>);
+
+impl<T: Sized> Deref for DynamicBoundFunction<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &*self.0
+    }
+}
+
+impl<T> DynamicBoundFunction<T> {
+    pub(crate) const fn new(init: fn() -> T) -> Self {
+        Self(Lazy::new(init))
+    }
+    pub(crate) fn init(&self) {
+        Lazy::force(&self.0);
+    }
+}
+
+
+pub(crate) static SCANNER: Lazy<ForceSyncSend<Scanner>> = Lazy::new(|| {
+    ForceSyncSend(auxtools::sigscan::Scanner::for_module(auxtools::BYONDCORE).unwrap())
 });
 
 pub fn find_by_call(scanner: &Scanner, signature: ExSignature) -> *mut u8 {
@@ -32,7 +99,7 @@ pub fn find_by_reference(scanner: &Scanner, signature: ExSignature) -> *mut u8 {
     let marker_pos = signature.marker_pos;
     let result = positions.iter()
         .map(|pointer| unsafe {
-            pointer.add(marker_pos as usize)
+            *(pointer.add(marker_pos as usize) as *const *mut u8)
         })
         .dedup()
         .collect::<Vec<_>>();
