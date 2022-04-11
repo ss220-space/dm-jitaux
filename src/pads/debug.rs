5b90262bd66994ab7b4b0d4fcd61d9b08250c3f9
simon.ogorodnik <simon.ogorodnik@jetbrains.com>
2022-04-11 16:36:33 +0300

    Revert

--- src/pads/debug.rs
+++ src/pads/debug.rs
@@ -2,7 +2,6 @@ use std::ffi::CStr;
 
 use auxtools::{sigscan, Value};
 use auxtools::raw_types::values::{ValueData, ValueTag};
-use crate::pads::{byond_imports, find_by_reference};
 
 use crate::pads::lists::get_list;
 
@@ -23,15 +22,14 @@ pub extern "C" fn handle_debug_val(val: ValueRaw) {
     log::debug!("dbg: {:#X} {}", val.tag, unsafe { val.data.id })
 }
 
-byond_imports!(
-    var DATUM_ARRAY_PTR: *mut *mut u8
-        = find_by_reference!(
-            unix    => "8b 15 >?? ?? ?? ?? 8b 14 82 85 d2 74 ad 8b 4a 18",
-        );
-);
+pub static mut DATUM_ARRAY_PTR: *mut *mut *mut u8 = std::ptr::null_mut();
 
 pub fn init() {
-    init_byond_imports();
+    let scanner = auxtools::sigscan::Scanner::for_module(auxtools::BYONDCORE).unwrap();
+
+    unsafe {
+        DATUM_ARRAY_PTR = *((scanner.find(signature!("8b 15 ?? ?? ?? ?? 8b 14 82 85 d2 74 ad 8b 4a 18")).unwrap()).add(2) as *mut *mut *mut *mut u8);
+    }
 }
 
 pub fn get_ref_count(value: Value) -> u32 {
@@ -39,7 +37,8 @@ pub fn get_ref_count(value: Value) -> u32 {
         log::debug!("{:?}", value.raw.tag);
         log::debug!("{:?}", value);
         return if value.raw.tag == ValueTag::Datum {
-            let datum_ptr = *(DATUM_ARRAY_PTR.add(value.raw.data.id as usize));
+            let array_ptr = *DATUM_ARRAY_PTR;
+            let datum_ptr = *(array_ptr.add(value.raw.data.id as usize));
             let res = *(datum_ptr.add(0x18) as *mut u32);
             log::debug!("datum ref count: {}", res);
             res
